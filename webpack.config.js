ef4cfc9c74553e9c41b3a31438a2469256af312f
Yaroslav Putria <yaroslav.putria@gmail.com>
2016-08-02 23:53:15 +0300

    added webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
     entry: "./src/app.js",
     output: {
@@ -6,15 +5,3 @@ module.exports = {
         filename: "bundle.js"
     }
 };
-=======
-'use strict';
-
- module.exports = {
-     entry: './src/app.js',
-     output: {
-         path: './',
-         filename: 'bundle.js'
-     }
- };
- 
->>>>>>> 67ed928b6a5647aa2d7a5d14cf92397ca614cb1e
