b00eb063f281530dd50d95aacf2845ec46cae53c
Yaroslav Putria <yaroslav.putria@gmail.com>
2016-08-02 23:37:59 +0300

    added webpack

---
+++ bundle.js
@@ -0,0 +1,85 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var View = __webpack_require__(1);
+	var Model = __webpack_require__(2);
+	var model = new Model();
+	model.init();
+	var view = new View();
+
+	view.render(model);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	function View () {
+
+	}
+
+	View.prototype.render = function () {
+	  console.log('Render');
+	};
+
+	module.exports = View;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	function Model () {
+
+	}
+
+	Model.prototype.init = function () {
+	  console.log('Inited');
+	};
+
+	module.exports = Model;
+
+/***/ }
+/******/ ]);
