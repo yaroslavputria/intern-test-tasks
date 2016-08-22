8dcd9a221536bd3ffc3f0766918ff9ea96ce7918
yaroslav.putria <yaroslav.putria@avid.com>
2016-08-22 14:57:48 +0300

    add two methods in model

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ window.require(
 	function(Model, View){
 		var model = new Model();
 		model.init();
+		model.getAllGists("my8bit")
+		.then(function(gists) {
+			console.log(gists);
+			var filteredGists = model.filterGists(gists, "application/json", "JSON");
+			console.log(filteredGists);
+		});
 		
 		var view = new View();
 		view.render(model);
--- src/model/model.js
+++ src/model/model.js
@@ -9,6 +9,25 @@ window.define(
 		Model.prototype.init = function () {
 		  console.log('Inited');
 		};
+
+		Model.prototype.getAllGists = function (name) {
+			return fetch("https://api.github.com/users/" + name + "/gists", {
+				method: "GET",
+				mode: 'cors'
+			})
+			.then(res => res.json());
+		};
+
+		Model.prototype.filterGists = function (arrOfGists, type, lang) {
+			return arrOfGists.filter(function(gist) {
+				for(var prop in gist.files) {
+					if (gist.files[prop].type === type && gist.files[prop].language === lang) {
+						return true;
+					} else return false;
+				}; 
+			});
+		};
+ 
 		return Model;
 	}
 );
