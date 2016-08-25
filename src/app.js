13f9d76daff78ae67e1aa7ee3cc18e7d9a2e40b0
yaroslav.putria <yaroslav.putria@avid.com>
2016-08-25 10:19:06 +0300

    fixed for code style

--- src/app.js
+++ src/app.js
@@ -1,43 +1,42 @@
 'use strict';
+require(['model/model', 'view/view'], function (Model, View) {
+  var model = new Model();
+  var view = new View();
+  model.init();
+  /*
+  model.getAllGists('my8bit')
+  .then(function(gists) {
+  	console.log(gists);
+  	var filteredGists = model.filterFiles(gists, 'application/json', 'JSON');
+  	console.log(filteredGists);
+  });
+  */
+  view.render(model);
+  var sendReq = document.getElementById('sendRequest');
+  sendReq.addEventListener('click', function () {
+    var userName = document.getElementById('userName');
+    var type = document.getElementById('type');
+    var lang = document.getElementById('lang');
+    model.getAllGists(userName.value)
+    .then(function (gists) {
+      console.log(gists);
+      var filteredGists = model.filterFiles(gists, type.value, lang.value);
+      var fileNames = model.filterByName(filteredGists);
+      console.log(fileNames);
+      tmpAppendListOfName(fileNames);
+    });
+  });
 
-window.require(
-	['model/model', 'view/view'],
-	function(Model, View){
-		var model = new Model();
-		model.init();
-		// model.getAllGists('my8bit')
-		// .then(function(gists) {
-		// 	console.log(gists);
-		// 	var filteredGists = model.filterFiles(gists, 'application/json', 'JSON');
-		// 	console.log(filteredGists);
-		// });
-		var view = new View();
-		view.render(model);
-		var sendReq = document.getElementById('sendRequest');
-		sendReq.addEventListener('click', function() {
-			var userName = document.getElementById('userName');
-			var type = document.getElementById('type');
-			var lang = document.getElementById('lang');
-			model.getAllGists(userName.value)
-			.then(function(gists) {
-				console.log(gists);
-				var filteredGists = model.filterFiles(gists, type.value, lang.value);
-				var fileNames = model.filterByName(filteredGists);
-				console.log(fileNames);
-				tmpAppendListOfName(fileNames);
-			});
-		});
+  function tmpAppendListOfName(list) {
+    var ul = document.createElement('ul');
+    ul.textContent = 'File(s) with chosen criteria:';
+    list.map(function (item) {
+      var tmpLi = document.createElement('li');
+      tmpLi.textContent = item;
+      ul.appendChild(tmpLi);
+    });
 
-		function tmpAppendListOfName(list) {
-			var ul = document.createElement('ul');
-			ul.textContent = 'File(s) with chosen criteria:';
-			list.map(function(item) {
-				var tmpLi = document.createElement('li');
-				tmpLi.textContent = item;
-				ul.appendChild(tmpLi);
-			});
-			document.body.appendChild(ul);
-		}
+    document.body.appendChild(ul);
+  }
 
-	}
-);
+});
--- src/model/model.js
+++ src/model/model.js
@@ -1,98 +1,78 @@
 'use strict';
+define(function () {
+  function Model() {
 
-define(
-	function() {
-		function Model () {
+  }
 
-		}
+  Model.prototype.init = function () {
+    console.log('Inited');
+  };
 
-		Model.prototype.init = function () {
-		  console.log('Inited');
-		};
+  Model.prototype.getAllGists = function (name) {
+    return fetch('https://api.github.com/users/' + name + '/gists', {
+      method: 'GET',
+      mode: 'cors',
+    })
+    .then(res => res.json());
+  };
 
-		Model.prototype.getAllGists = function (name) {
-			return fetch('https://api.github.com/users/' + name + '/gists', {
-				method: 'GET',
-				mode: 'cors'
-			})
-			.then(res => res.json());
-		};
+  Model.prototype.filterFiles = function (arrOfGists, type, lang) {
+    if ({}.toString.call(arrOfGists).slice(8, -1) === 'Array') {
+      var arrOfFiles = [];
+      if (type) {
+        if (lang) {
+          arrOfGists.forEach(function (gist) {
+            for (var prop in gist.files) {
+              if (gist.files.hasOwnProperty(prop)) {
+                if (gist.files[prop].type === type && gist.files[prop].language === lang) {
+                  arrOfFiles.push(gist.files[prop]);
+                }
+              }
+            }
+          });
+        } else {
+          arrOfGists.forEach(function (gist) {
+            for (var prop in gist.files) {
+              if (gist.files.hasOwnProperty(prop)) {
+                if (gist.files[prop].type === type) {
+                  arrOfFiles.push(gist.files[prop]);
+                }
+              }
+            }
+          });
+        }
+      } else {
+        if (lang) {
+          arrOfGists.forEach(function (gist) {
+            for (var prop in gist.files) {
+              if (gist.files.hasOwnProperty(prop)) {
+                if (gist.files[prop].language === lang) {
+                  arrOfFiles.push(gist.files[prop]);
+                }
+              }
+            }
+          });
+        } else {
+          arrOfGists.forEach(function (gist) {
+            for (var prop in gist.files) {
+              if (gist.files.hasOwnProperty(prop)) {
+                arrOfFiles.push(gist.files[prop]);
+              }
+            }
+          });
+        }
+      }
 
-		Model.prototype.filterFiles = function (arrOfGists, type, lang) {
-			if ({}.toString.call(arrOfGists).slice(8, -1) === 'Array') {
-				var arrOfFiles = [];
-				// arrOfGists.forEach(function(gist) {
-				// 	for(var prop in gist.files) {
-				// 		if (type && gist.files[prop].type === type) {
-				// 			if (lang && gist.files[prop].language === lang) {
-				// 				arrOfFiles.push(gist.files[prop]);
-				// 			} else {
-				// 				arrOfFiles.push(gist.files[prop]);
-				// 			};
-				// 		} else if (lang && gist.files[prop].language === lang){
-				// 			arrOfFiles.push(gist.files[prop]);
-				// 		} else {
-				// 			arrOfFiles.push(gist.files[prop]);
-				// 		};
-				// 	}; 
-				// });
+      return arrOfFiles;
+    } else {
+      throw new Error('wrong input data');
+    }
+  };
 
-				if (type) {
-					if (lang) {
-						arrOfGists.forEach(function(gist) {
-							for(var prop in gist.files) {
-								if (gist.files.hasOwnProperty(prop)){
-									if (gist.files[prop].type === type && gist.files[prop].language === lang){
-										arrOfFiles.push(gist.files[prop]);
-									}
-								}
-							}
-						});
-					} else {
-						arrOfGists.forEach(function(gist) {
-							for(var prop in gist.files) {
-								if (gist.files.hasOwnProperty(prop)){
-									if (gist.files[prop].type === type){
-										arrOfFiles.push(gist.files[prop]);
-									}
-								}
-							}
-						});
-					}
-				} else {
-					if (lang){
-						arrOfGists.forEach(function(gist) {
-							for(var prop in gist.files) {
-								if (gist.files.hasOwnProperty(prop)){
-									if (gist.files[prop].language === lang){
-										arrOfFiles.push(gist.files[prop]);
-									}
-								}
-							}
-						});
-					} else {
-						arrOfGists.forEach(function(gist) {
-							for (var prop in gist.files) {
-								if (gist.files.hasOwnProperty(prop)){
-									arrOfFiles.push(gist.files[prop]);
-								}
-							}
-						});
-					}
-				}
-				return arrOfFiles;
-			} else {
-				throw new Error('wrong input data');
-			}
-		};
- 		
-		Model.prototype.filterByName = function (arrOfFiles) {
-			return arrOfFiles.map(file => {
-				return file.filename;
-			});
-		};
+  Model.prototype.filterByName = function (arrOfFiles) {
+    return arrOfFiles.map(file => file.filename);
+  };
 
-		return Model;
-
-	}
+  return Model;
+}
 );
